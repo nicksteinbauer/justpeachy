76239cdecd48b07662a7552e14a56cc5f3d5f97f
Nicholas Steinbauer <web@obrien.com>
2024-02-10 13:16:24 -0500

    favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 //import Image from 'next/image'
 import Hero from "./components/Hero"
-import Header from "./components/Header"
 import AboutComp from "./components/AboutComp"
 import SmGallery from "./components/SmGallery"
 import Widget from "./components/Widget"
@@ -8,7 +7,6 @@ import Amenities from "./components/Amenities"
 export default function Home() {
   return (
     <main className="main">
-      
       <Hero />
       <div className="theRest">
         <Widget />
