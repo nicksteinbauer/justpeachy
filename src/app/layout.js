61a82355db93838430ff372bce3d3142e8ecf3f6
Nicholas Steinbauer <web@obrien.com>
2024-04-05 14:12:28 -0400

    more seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,8 @@ const open = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Marblehead Ohio Cottage Rental - Justy Peachy Marblehead Cottage',
-  description: 'Marblehead Ohio AirBnB Cottage near Lakeside Ohio',
+  description: 'Discover the perfect Marblehead Ohio cottage rental for your next family vacation. Centrally located near Lakeside and various attractions, our spacious home offers comfort and convenience for memorable getaways.',
+  keywords: 'Marblehead Ohio, cottage rental, vacation rental, Lake Erie islands, Marblehead Peninsula, Lakeside, Kelleys Island Ferry, Marblehead Lighthouse State Park',
 }
 
 export default function RootLayout({ children }) {
